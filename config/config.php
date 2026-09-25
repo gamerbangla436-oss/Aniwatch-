12c3478cce44191fddf5e8385f23e1dc5bc3ae2e
gamerbangla436-oss <gamerbangla436@gmail.com>
2026-09-25 20:19:10 +0600

    Create default runtime config for AniRead so the site can connect to MySQL and load in standard shared hosting setups.

---
+++ config/config.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'db' => [
+        'host' => 'localhost',
+        'name' => 'aniread',
+        'user' => 'root',
+        'pass' => '',
+        'charset' => 'utf8mb4',
+    ],
+    'site_url' => 'http://localhost/aniread',
+    'site_name' => 'AniRead',
+    'timezone' => 'UTC',
+    'jikan_base' => 'https://api.jikan.moe/v4',
+    'sync_limit' => 12,
+];
